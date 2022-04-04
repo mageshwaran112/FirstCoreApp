0d9d128a3b2eceb1bdaec9ba0cdd5fc9e1b40d6a
mageshwaran112 <jsquaresoftware@gmail.com>
2022-04-04 22:18:54 +0530

    Books Models

---
+++ FirstCoreModelApi/Common/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCoreModelApi.Common
+{
+    public class Books
+    {
+        public Int32 Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public Int32? Rate { get; set; }
+        public string General { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAddded { get; set; }
+
+    }
+}
